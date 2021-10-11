946f8b560b896ccf3defcded0ee234aae905093b
Jan Lindblom <jan@namnlos.io>
2021-10-11 11:14:23 +0300

    added main folder

--- keyboards/lotus58/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "config_common.h"
-
-#define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 5
--- keyboards/lotus58/lotus58.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#ifdef KEYBOARD_lotus58_rev1
-    #include "rev1.h"
-#endif
