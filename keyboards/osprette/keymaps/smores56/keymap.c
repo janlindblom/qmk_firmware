1638c8ebe2d4bd9c41c3f7de4c245c8d30af2b01
Sam Mohr <sam.mohr@protonmail.com>
2021-10-20 12:59:45 -0400

    Move to CTGAP

--- keyboards/osprette/keymaps/smores56/keymap.c
+++ keyboards/osprette/keymaps/smores56/keymap.c
@@ -42,38 +42,39 @@ enum layers {
 #define RGUI_ OSM(MOD_RGUI)
 #define RSG_  OSM(MOD_RSFT | MOD_RGUI)
 #define RAG_  OSM(MOD_RALT | MOD_RGUI)
+#define RCA_  OSM(MOD_RCTL | MOD_RALT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAIN] = LAYOUT(
-                     KC_C,  KC_L,  KC_M, KC_K,         KC_Z,  KC_F, KC_U,    KC_COMM,
-        KC_Y, KC_I,  KC_S,  KC_R,  KC_T, KC_G,         KC_P,  KC_N, KC_E,    KC_A,    KC_O, KC_SLSH,
-              KC_Q,  KC_V,  KC_W,  KC_D, KC_J,         KC_B,  KC_H, KC_QUOT, KC_DOT,  KC_X,
-                                   LMOD, SPC_NAV,      RSFT_, RMOD
+                     KC_P,  KC_L,  KC_C, KC_F,         KC_K,    KC_U,    KC_O,   KC_Y,
+        KC_V, KC_R,  KC_N,  KC_T,  KC_S, KC_D,         KC_QUOT, KC_A,    KC_E,   KC_I,    KC_H,    KC_Q,
+              KC_Z,  KC_B,  KC_M,  KC_G, KC_W,         KC_MINS, KC_COMM, KC_DOT, KC_SCLN, KC_SLSH,
+                                   LMOD, SPC_NAV,      RSFT_,   RMOD
     ),
 
     [_SYM_LMOD] = LAYOUT(
-                      C(KC_W),      C(KC_Q),   C(KC_T), G(KC_L),      KC_LBRC,    KC_DEL,   KC_RBRC,    S(KC_3),
-        KC_NO, LSFT_, LALT_,        LCTL_,     LGUI_,   KC_PSCR,      S(KC_9),    KC_BSPC,  S(KC_0),    S(KC_COMM), S(KC_BSLS), S(KC_5),
-               KC_NO, C(S(KC_TAB)), C(KC_TAB), KC_NO,   KC_CAPS,      S(KC_LBRC), KC_ENTER, S(KC_RBRC), S(KC_DOT),  S(KC_GRV),
-                                               KC_NO,   KC_NO,        KC_TRNS,    FN                                     
+                        KC_NO,   C(S(KC_TAB)), C(KC_TAB),  KC_NO,            S(KC_LBRC), KC_DEL,   S(KC_RBRC), S(KC_3),
+        KC_CAPS, LSFT_, LALT_,   LCTL_,        LGUI_,      KC_PSCR,          S(KC_9),    KC_BSPC,  S(KC_0),    S(KC_COMM), S(KC_2),   KC_BSLS,
+                 KC_NO, C(KC_W), C(KC_Q),      A(KC_LEFT), A(KC_RIGHT),      KC_LBRC,    KC_ENTER, KC_RBRC,    S(KC_DOT),  S(KC_GRV),
+                                               KC_NO,      KC_NO,            KC_TRNS,    FN
     ),
 
     [_NUM_RMOD] = LAYOUT(
-                          KC_7,  KC_8,  KC_9,   S(KC_8),        KC_NO, RSG_,  C(KC_D), C(KC_C),
-        S(KC_4), S(KC_7), KC_1,  KC_2,  KC_3,   KC_EQL,         KC_NO, RGUI_, RCTL_,   RALT_,   RSFT_, C(KC_V),
-                 S(KC_6), KC_4,  KC_5,  KC_6,   S(KC_EQL),      KC_NO, KC_NO, S(KC_2), KC_NO,   KC_NO,
-                                        KC_DOT, KC_0,           KC_NO, KC_NO
+                             KC_7,  KC_8,  KC_9,   S(KC_8),        KC_NO, RSG_,    KC_NO,  KC_NO,
+        S(KC_BSLS), S(KC_7), KC_1,  KC_2,  KC_3,   KC_EQL,         KC_NO, RGUI_,   RCTL_,   RALT_,   RSFT_, KC_NO,
+                    S(KC_4), KC_4,  KC_5,  KC_6,   S(KC_EQL),      KC_NO, C(KC_D), C(KC_C), C(KC_V), KC_NO,
+                                           KC_DOT, KC_0,           KC_NO, KC_NO
     ),
 
     [_NAV] = LAYOUT(
-                          KC_VOLD, KC_VOLU, KC_MNXT,  KC_MUTE,      KC_NO,   G(KC_D),     G(S(KC_D)), G(KC_Q),
-        KC_MPRV, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_MPLY,      G(KC_R), G(KC_LEFT),  G(KC_DOWN), G(KC_UP), G(KC_RIGHT), KC_NO,
-                 KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_MSTP,      KC_NO,   G(KC_ENTER), G(KC_O),    G(KC_N),  KC_NO,
-                                            KC_NO,    KC_NO,        KC_TRNS, KC_NO                                   
+                          KC_VOLD, KC_VOLU, KC_MNXT,  KC_MUTE,      RCTL_,   G(KC_D),     G(S(KC_D)), G(KC_Q),
+        KC_MPRV, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_MPLY,      RSG_,    G(KC_LEFT),  G(KC_DOWN), G(KC_UP), G(KC_RIGHT), KC_NO,
+                 KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_MSTP,      RALT_,   G(KC_ENTER), G(KC_O),    G(KC_N),  KC_NO,
+                                            KC_NO,    KC_NO,        KC_TRNS, RAG_
     ),
 
     [_FN] = LAYOUT(
-                      KC_F7, KC_F8, KC_F9, KC_F10,      KC_NO,   KC_BRIU, KC_BRIU, KC_NO,
+                      KC_F7, KC_F8, KC_F9, KC_F10,      KC_NO,   KC_BRID, KC_BRIU, KC_NO,
         KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F11,      KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
                KC_NO, KC_F4, KC_F5, KC_F6, KC_F12,      KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
                                     KC_NO, KC_NO,       KC_TRNS, KC_NO
@@ -81,43 +82,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 enum combos {
-    WD_TAB,
-    VW_ESC,
-    HQ_MINS,
-    HD_UNDER,
-    FD_GRAVE,
-    OS_BSLH,
-    UC_COLON,
-    FC_SCLN,
+    MG_TAB,
+    BM_ESCAPE,
+    PC_J,
+    BG_X,
+    CD_GRAVE,
+    DS_PERCENT,
 };
 
-const uint16_t PROGMEM tab_combo[] = {KC_W, KC_D, COMBO_END};
-const uint16_t PROGMEM enter_combo[] = {KC_V, KC_W, COMBO_END};
-const uint16_t PROGMEM escape_combo[] = {KC_V, KC_W, COMBO_END};
-const uint16_t PROGMEM minus_combo[] = {KC_H, KC_QUOT, COMBO_END};
-const uint16_t PROGMEM underscore_combo[] = {KC_H, KC_DOT, COMBO_END};
-const uint16_t PROGMEM grave_combo[] = {KC_QUOT, KC_DOT, COMBO_END};
-const uint16_t PROGMEM backslash_combo[] = {KC_O, KC_SLSH, COMBO_END};
-const uint16_t PROGMEM colon_combo[] = {KC_U, KC_COMM, COMBO_END};
-const uint16_t PROGMEM semicolon_combo[] = {KC_F, KC_COMM, COMBO_END};
+const uint16_t PROGMEM tab_combo[] = {KC_M, KC_G, COMBO_END};
+const uint16_t PROGMEM escape_combo[] = {KC_B, KC_M, COMBO_END};
+const uint16_t PROGMEM j_combo[] = {KC_P, KC_C, COMBO_END};
+const uint16_t PROGMEM x_combo[] = {KC_B, KC_G, COMBO_END};
+const uint16_t PROGMEM grave_combo[] = {KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM percent_combo[] = {KC_DOT, KC_SCLN, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-    [WD_TAB]    = COMBO(tab_combo, KC_TAB),
-    [VW_ESC]    = COMBO(escape_combo, KC_ESC),
-    [HQ_MINS]   = COMBO(minus_combo, KC_MINS),
-    [HD_UNDER]  = COMBO(underscore_combo, S(KC_MINS)),
-    [FD_GRAVE]  = COMBO(grave_combo, KC_GRAVE),
-    [OS_BSLH]   = COMBO(backslash_combo, KC_BSLS),
-    [UC_COLON]  = COMBO(colon_combo, S(KC_SCLN)),
-    [FC_SCLN]   = COMBO(semicolon_combo, KC_SCLN),
+    [MG_TAB]     = COMBO(tab_combo, KC_TAB),
+    [BM_ESCAPE]  = COMBO(escape_combo, KC_ESC),
+    [PC_J]       = COMBO(j_combo, KC_J),
+    [BG_X]       = COMBO(x_combo, KC_X),
+    [CD_GRAVE]   = COMBO(grave_combo, KC_GRAVE),
+    [DS_PERCENT] = COMBO(percent_combo, S(KC_5)),
 };
 
 const key_override_t question_mark_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, S(KC_SLSH));
 const key_override_t exclamation_mark_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, S(KC_1));
+const key_override_t forward_slash_override = ko_make_basic(MOD_MASK_SHIFT, KC_SLSH, S(KC_6));
 
-// This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
     &question_mark_override,
     &exclamation_mark_override,
-    NULL // Null terminate the array of overrides!
+    &forward_slash_override,
+    NULL
 };
