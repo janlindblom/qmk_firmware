2ddb14e7f2cbea527ed408b2c40d025b4f0c5f84
Jan Lindblom <jan@namnlos.co>
2023-06-20 22:10:31 +0300

    More tweaks to layer display on OLED.

--- users/namnlos/namnlos.h
+++ users/namnlos/namnlos.h
@@ -208,14 +208,10 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation);
 #        endif
 #    else // This is a limited width display (like a 32x128 OLED)
 #        define OLED_RENDER_LAYOUT_QWERTY "QWRTY"
-<<<<<<< HEAD
-
-=======
->>>>>>> 4bdbaf6c2e (Tweaking things and adding Svorak support.)
 #        define OLED_RENDER_LAYOUT_ADESK "ADesk"
 #        define OLED_RENDER_LAYOUT_KICAD "KiCad"
 #        define OLED_RENDER_LAYOUT_GAMING "Game "
-#        define OLED_RENDER_LAYOUT_SVORAK "VORAK"
+#        define OLED_RENDER_LAYOUT_SVORAK "SVRAK"
 #        define OLED_RENDER_LAYER_LOWER "Lower"
 #        define OLED_RENDER_LAYER_SYMB "Symb "
 #        define OLED_RENDER_LAYER_NUMB "Numb "
