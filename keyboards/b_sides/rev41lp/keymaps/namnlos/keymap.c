fdd3a8f7ef9320f5875bc1b88d38601b07667d9f
Jan Lindblom <jan@namnlos.co>
2023-08-17 11:38:38 +0300

    Updates to rev41lp keymap.

--- keyboards/b_sides/rev41lp/keymaps/namnlos/keymap.c
+++ keyboards/b_sides/rev41lp/keymaps/namnlos/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright © 2022 Jan Lindblom (@janlindblom)
+/* Copyright © 2023 Jan Lindblom (@janlindblom)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -70,59 +70,55 @@ const uint32_t PROGMEM unicode_map[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
-    [_BASE] = LAYOUT_rev41lp(
+    [_BASE] = LAYOUT(
         CK_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  SE_ARNG,
         CK_BSPC,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,  SE_ODIA,  CK_ADIA,
         CK_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,  SE_COMM,   SE_DOT,  SE_MINS,  CK_QUOT,
                                               CK_DEL,   CK_ENT1,  CK_SPC,   CK_ENT2,   CK_TAB
     ),
 
-    [_LOWER] = LAYOUT_rev41lp(
+    [_LOWER] = LAYOUT(
     SK_NOT_EQL,  SE_EXLM,    SE_AT,  SE_LCBR,  SE_RCBR,   SE_PIPE,             SE_PND,  SE_EURO,  SE_QUES,  SE_SECT,  SE_CURR,  SE_DIAE,
         SK_LEQ,  SE_HASH,   SE_DLR,  SE_LPRN,  SE_RPRN,    SE_GRV,            SE_PLUS,  SE_MINS,  SE_SLSH,  SE_ASTR,  SE_PERC,  SE_DQUO,
         SK_GEQ,  SE_PERC,  SE_CIRC,  SE_LBRC,  SE_RBRC,   SE_TILD,            SE_AMPR,   SE_EQL,  SE_LABK,  SE_RABK,  SE_BSLS,  SE_ACUT,
                                                _______,   _______,  _______,  _______,  _______
     ),
 
-    [_RAISE] = LAYOUT_rev41lp(
-        SE_SECT,   SE_1,     SE_2,     SE_3,     SE_4,     SE_5,                 SE_6,    SE_7,    SE_8,    SE_9,    SE_0,  KC_MPLY,
+    [_RAISE] = LAYOUT(
+        SE_SECT,   SE_1,     SE_2,     SE_3,     SE_4,     SE_5,                 SE_6,    SE_7,    SE_8,    SE_9,    SE_0,  KC_MUTE,
         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,              KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_PGUP,  KC_VOLU,
         KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,              CK_WLFT, CK_WRGT, KC_HOME,  KC_END, KC_PGDN,  KC_VOLD,
                                                 KC_BSPC,  _______,  KC_ENT,  _______,  _______
     ),
 
-    [_ADJST] = LAYOUT_rev41lp(
+    [_ADJST] = LAYOUT(
         XXXXXXX,   XXXXXXX, XXXXXXX,   BL_OFF,  BL_DOWN,   BL_TOGG,            BL_BRTG, BL_UP,   BL_ON,   XXXXXXX, XXXXXXX,  XXXXXXX,
-        XXXXXXX,  KC_ADESK,KC_KICAD,  XXXXXXX,   SK_BLL, KC_GAMING,            NK_ON,   KO_ON,   AC_ON,   AS_ON,   XXXXXXX,  XXXXXXX,
+        XXXXXXX,  CK_ADESK,CK_KICAD,  XXXXXXX,   SK_BLL, CK_GAMING,            NK_ON,   KO_ON,   AC_ON,   AS_ON,   XXXXXXX,  XXXXXXX,
         XXXXXXX,   UC_WINC,  UC_MAC,  UC_LINX,  XXXXXXX,   QK_BOOT,            NK_OFF,  KO_OFF,  AC_OFF,  AS_OFF,  XXXXXXX,  XXXXXXX,
                                                 _______,   _______, _______,   _______,  _______
     ),
     // KiCAD top layer for single hand use.
-    [_KICAD] = LAYOUT_rev41lp(
-         KC_ESC,     KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,            KC_Y,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_QWERTY,
+    [_KICAD] = LAYOUT(
+         KC_ESC,     KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,            KC_Y,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  CK_QWERTY,
         KC_LCTL,     KC_A,     KC_L,     KC_D,     KC_M,      KC_G,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
         KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
                                                  CK_DEL,   CK_ENT1,  KC_SPC,   CK_ENT2,   CK_TAB
     ),
     // AutoDesk (Fusion 360) top layer for single hand use.
-    [_ADESK] = LAYOUT_rev41lp(
-         KC_ESC,      KC_I,    KC_M,     KC_E,     KC_R,      KC_T,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_QWERTY,
+    [_ADESK] = LAYOUT(
+         KC_ESC,      KC_I,    KC_M,     KC_E,     KC_R,      KC_T,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  CK_QWERTY,
         KC_LCTL,      KC_A,    KC_S,     KC_D,     KC_F,      KC_J,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
         KC_LSFT,      KC_P,    KC_X,     KC_C,     KC_V,      KC_B,            XXXXXXX,  KC_M,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
                                                  CK_DEL,   CK_ENT1,  KC_SPC,   CK_ENT2,   CK_TAB
     ),
-    [_GAMING] = LAYOUT_rev41lp(
+    [_GAMING] = LAYOUT(
         KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  SE_ARNG,
         KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,  SE_ODIA,  SE_ADIA,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,  SE_COMM,   SE_DOT,  SE_MINS,  CK_QUOT,
-                                              KC_DEL,    KC_ENT,  KC_SPC,   CK_ENT1,  KC_QWERTY
+                                              KC_DEL,    KC_ENT,  KC_SPC,   CK_ENT1,  CK_QWERTY
     )
     // clang-format on
 };
-
-layer_state_t layer_state_set_keymap(layer_state_t state) {
-    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJST);
-}
 
 // Combos, if enabled
 #ifdef COMBO_ENABLE
