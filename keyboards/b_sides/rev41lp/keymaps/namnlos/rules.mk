534de4dd4ee67d5b30d44d3fda6aeffc0e8d6dc2
Jan Lindblom <jan@namnlos.co>
2023-08-17 11:38:38 +0300

    Updates to rev41lp keymap.

--- keyboards/b_sides/rev41lp/keymaps/namnlos/rules.mk
+++ keyboards/b_sides/rev41lp/keymaps/namnlos/rules.mk
@@ -7,9 +7,10 @@ UNICODEMAP_ENABLE    = yes
 CONSOLE_ENABLE       = no
 OLED_ENABLE          = no
 KEY_OVERRIDE_ENABLE  = yes
-COMMAND_ENABLE       = yes
+COMMAND_ENABLE       = no
 AUTOCORRECT_ENABLE   = yes
 NKRO_ENABLE          = yes
 DEFERRED_EXEC_ENABLE = yes
+LTO_ENABLE           = yes
 
 PROMICRO_LED_STATUS  = yes      # Use the onboard LED's on the ProMicro for status
